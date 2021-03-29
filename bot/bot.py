e46ead56f1d4712f95de12a2fb488af63589f0a7
Chris <maherc7@tcd.ie>
2021-03-29 16:31:28 +0100

    Added basic member timing game
    users can start a round by tagging a member and adding a time, the bot will setup a round and await their arrival. When the user arrives the bot will end the round and send a message with the time statistics

--- bot/bot.py
+++ bot/bot.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from dateutil.parser import parse
 from discord.ext import commands
 from dotenv import load_dotenv
+from game import Game
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -17,42 +18,82 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 bot.sandwich_id = 326471661928972299
 
+bot.games = {}
+
 # When person connects to the voice channel
 @bot.event
 async def on_voice_state_update(member, before, after):
     
-    if (not after.channel): # ignore users leaving a channel
-        return
-    if (before.channel): 
-        if (after.channel.name == before.channel.name): # ignore users already in the channel
-            return
+    if before.channel is None and after.channel is not None:
+        if after.channel.name == 'General':
+            
+            if member.guild.id in bot.games:
+                
+                game = bot.games[member.guild.id]
+                if member == game.member:
+                    #player has arrived and game is over
 
-    if (after.channel.name == 'General'):
+                    #calculate time
+                    
+                    timedelta = (game.proposedtime - datetime.now()).total_seconds()
+
+                    if timedelta > 0:
+                        result_msg = f"{member.name} is early by {int(timedelta / 60)} minutes and {int(timedelta % 60)} seconds! Congrats!"
+                    elif timedelta < 0:
+                        timedelta *= -1
+                        result_msg = f"{member.name} is late by {int(timedelta / 60)} minutes and {int(timedelta % 60)} seconds! Boo!"
+                    elif timedelta == 0:
+                        result_msg = f"{member.name} is exactly on time!"
 
 
-        channel = discord.utils.get(member.guild.text_channels, name='bot-commands')
+                    #setup embed
 
-        await channel.send(f"{member.name} just joined #General, I'm watching you")
+
+                    embedVar = discord.Embed(title="Round Ended", description=f"Member {member.name} has arrived", color=0x00ff00)
+                    embedVar.add_field(name="Promised time", value=game.proposedtime.strftime("%H:%M:%S"), inline=False)
+                    embedVar.add_field(name="Current time", value=datetime.now().strftime("%H:%M:%S"), inline=False)
+                    embedVar.add_field(name="Result", value=result_msg, inline=False)
+                    
+
+
+
+                    await game.command_channel.send(embed=embedVar)
+
+
 
 # Bot commands
 @bot.command(name='new_round', help = 'Start new round of betting for his arrival time.')
-async def new_round(ctx, proposed_time):
+async def new_round(ctx, member: discord.Member, proposed_time):
+    #TODO: except if member is already in vc
 
-    
     proposed_time_parsed = parse(proposed_time)
     proposed_time_formatted = proposed_time_parsed.strftime("%H:%M:%S")
 
+
     round_start_time = datetime.now().strftime("%H:%M:%S")
 
-    await ctx.send(
-        f"""New Round.
-        \nThe sandwich has stated his time of arrival to be {proposed_time_formatted}.
-        \nCurrent time: {round_start_time}."""
-    )
+    
+    if ctx.guild.id in bot.games:
+        await ctx.send("Sorry! A round is already in progress")
+        return
+    else: 
+        bot.games[ctx.guild.id] = Game(member, datetime.now(), proposed_time_parsed, ctx.channel)
 
-    vc = discord.utils.get(ctx.guild.voice_channels, name='General')
-    members = '\n - '.join([member.name for member in vc.members])
-    await ctx.send(f"Users in voice chat: \n {members}")
+    
+    
+    embedVar = discord.Embed(title="New Round", description=f"Member {member.name} is being timed!", color=0x00ff00)
+    embedVar.add_field(name="Promised time", value=proposed_time_formatted, inline=False)
+    embedVar.add_field(name="Current time", value=round_start_time, inline=False)
+
+
+
+    await ctx.send(embed=embedVar)
+
+
+# TODO: restore as a check memebers command 
+    # vc = discord.utils.get(ctx.guild.voice_channels, name='General')
+    # members = '\n - '.join([member.name for member in vc.members])
+    # await ctx.send(f"Users in voice chat: \n {members}")
 
 @bot.event
 async def on_command_error(ctx, error):
---
+++ bot/game.py
@@ -0,0 +1,6 @@
+class Game:
+    def __init__(self, member, starttime, proposedtime, channel):
+        self.member = member
+        self.starttime = starttime
+        self.proposedtime = proposedtime
+        self.command_channel = channel

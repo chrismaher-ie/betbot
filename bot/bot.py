93902df2182912df893aea7aa44331b226f51e02
Chris <maherc7@tcd.ie>
2021-03-29 14:54:13 +0100

    Changed bot channel and sandwich_id to a bot parameter

--- bot/bot.py
+++ bot/bot.py
@@ -10,12 +10,12 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-sandwich_id = 233699368249720832
-
 intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+bot.sandwich_id = 326471661928972299
 
 # When person connects to the voice channel
 @bot.event
@@ -30,7 +30,7 @@ async def on_voice_state_update(member, before, after):
     if (after.channel.name == 'General'):
 
 
-        channel = discord.utils.get(member.guild.text_channels, name='general')
+        channel = discord.utils.get(member.guild.text_channels, name='bot-commands')
 
         await channel.send(f"{member.name} just joined #General, I'm watching you")
 
@@ -38,6 +38,7 @@ async def on_voice_state_update(member, before, after):
 @bot.command(name='new_round', help = 'Start new round of betting for his arrival time.')
 async def new_round(ctx, proposed_time):
 
+    
     proposed_time_parsed = parse(proposed_time)
     proposed_time_formatted = proposed_time_parsed.strftime("%H:%M:%S")
 

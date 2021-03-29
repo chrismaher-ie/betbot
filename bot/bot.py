6bed0bcf6653f68b806ba3d1befde2acded774bb
Chris <maherc7@tcd.ie>
2021-03-30 00:29:08 +0100

    Added finish_round() and functionified round end logic

--- bot/bot.py
+++ bot/bot.py
@@ -20,39 +20,47 @@ bot.sandwich_id = 326471661928972299
 
 bot.games = {}
 
+#TODO: move to game.py
+def get_round_end_embed(game):
+                    
+                    
+    #calculate time
+    timedelta = (game.proposedtime - datetime.now()).total_seconds()
+    if timedelta > 0:
+        result_msg = f"{game.member.name} is early by {int(timedelta / 60)} minutes and {int(timedelta % 60)} seconds! Congrats!"
+    elif timedelta < 0:
+        timedelta *= -1
+        result_msg = f"{game.member.name} is late by {int(timedelta / 60)} minutes and {int(timedelta % 60)} seconds! Boo!"
+    elif timedelta == 0:
+        result_msg = f"{game.member.name} is exactly on time!"
+    
+    #setup embed to display results
+    embedVar = discord.Embed(title="Round Ended", description=f"Member {game.member.name} has arrived", color=0x00ff00)
+    embedVar.add_field(name="Promised time", value=game.proposedtime.strftime("%H:%M:%S"), inline=False)
+    embedVar.add_field(name="Current time", value=datetime.now().strftime("%H:%M:%S"), inline=False)
+    embedVar.add_field(name="Result", value=result_msg, inline=False)
+
+    return embedVar
+
+
 # When person connects to the voice channel
 @bot.event
 async def on_voice_state_update(member, before, after):
     #TODO: logic below is not robust and only suitable for testing, fix to handle more cases
     if before.channel is None and after.channel is not None:
-        if after.channel.name == 'General':
+        
+        if after.channel.guild.id in bot.games: #check if this guild is running a game
+            game = bot.games[member.guild.id]
             
-            if after.channel.guild.id in bot.games: #check if this guild is running a game
-                
-                if member == bot.games[member.guild.id].member: #player has arrived and game is over
-                    #TODO: change to game member function 
+            if after.channel.name == 'General': #change vaule to a variable attibute of Game if after.channel.name == game.voice_channel:
+            
+                if member == game.member: #player has arrived and game is over
                     
-                    game = bot.games[member.guild.id]
-                    del bot.games[member.guild.id]
-                    
-                    #calculate time
-                    timedelta = (game.proposedtime - datetime.now()).total_seconds()
-                    if timedelta > 0:
-                        result_msg = f"{member.name} is early by {int(timedelta / 60)} minutes and {int(timedelta % 60)} seconds! Congrats!"
-                    elif timedelta < 0:
-                        timedelta *= -1
-                        result_msg = f"{member.name} is late by {int(timedelta / 60)} minutes and {int(timedelta % 60)} seconds! Boo!"
-                    elif timedelta == 0:
-                        result_msg = f"{member.name} is exactly on time!"
-                    
-                    #setup embed to display results
-                    embedVar = discord.Embed(title="Round Ended", description=f"Member {member.name} has arrived", color=0x00ff00)
-                    embedVar.add_field(name="Promised time", value=game.proposedtime.strftime("%H:%M:%S"), inline=False)
-                    embedVar.add_field(name="Current time", value=datetime.now().strftime("%H:%M:%S"), inline=False)
-                    embedVar.add_field(name="Result", value=result_msg, inline=False)
-                    
+                    #TODO: change to game member function                    
+                    embedVar = get_round_end_embed( game )
                     await game.command_channel.send(embed=embedVar)
 
+                    del bot.games[member.guild.id]
 
 # Bot commands
 @bot.command(name='new_round', help = 'Start new round of betting for the member\'s arrival time.')
@@ -83,7 +91,16 @@ async def cancel_round(ctx):
     else:
         await ctx.send(embed=discord.Embed(Title="Error", description="Sorry! There is no round in progress to cancel.", color=0xff0000))
 
-        
+@bot.command(name='finish_round', help = 'Manually end current round of betting.')
+async def finish_round(ctx):
+    if ctx.guild.id in bot.games:
+        embedVar = get_round_end_embed( bot.games[ctx.guild.id] )
+
+        await ctx.send(embed=embedVar)
+
+        del bot.games[ctx.guild.id]
+
+
 # TODO: restore as a check members command or remove
     # vc = discord.utils.get(ctx.guild.voice_channels, name='General')
     # members = '\n - '.join([member.name for member in vc.members])

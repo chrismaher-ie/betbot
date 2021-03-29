f77cdb3304e0ff04792829f650bb243b2dbe3533
Chris <maherc7@tcd.ie>
2021-03-29 19:11:43 +0100

    Bug fixes and cleanup of on_voice_state_update() and new_round()

--- bot/bot.py
+++ bot/bot.py
@@ -23,20 +23,20 @@ bot.games = {}
 # When person connects to the voice channel
 @bot.event
 async def on_voice_state_update(member, before, after):
-    
+    #TODO: logic below is not robust and only suitable for testing, fix to handle more cases
     if before.channel is None and after.channel is not None:
         if after.channel.name == 'General':
             
-            if member.guild.id in bot.games:
+            if after.channel.guild.id in bot.games: #check if this guild is running a game
                 
-                game = bot.games[member.guild.id]
-                if member == game.member:
-                    #player has arrived and game is over
-
-                    #calculate time
+                if member == bot.games[member.guild.id].member: #player has arrived and game is over
+                    #TODO: change to game member function 
                     
+                    game = bot.games[member.guild.id]
+                    del bot.games[member.guild.id]
+                    
+                    #calculate time
                     timedelta = (game.proposedtime - datetime.now()).total_seconds()
-
                     if timedelta > 0:
                         result_msg = f"{member.name} is early by {int(timedelta / 60)} minutes and {int(timedelta % 60)} seconds! Congrats!"
                     elif timedelta < 0:
@@ -44,21 +44,14 @@ async def on_voice_state_update(member, before, after):
                         result_msg = f"{member.name} is late by {int(timedelta / 60)} minutes and {int(timedelta % 60)} seconds! Boo!"
                     elif timedelta == 0:
                         result_msg = f"{member.name} is exactly on time!"
-
-
-                    #setup embed
-
-
+                    
+                    #setup embed to display results
                     embedVar = discord.Embed(title="Round Ended", description=f"Member {member.name} has arrived", color=0x00ff00)
                     embedVar.add_field(name="Promised time", value=game.proposedtime.strftime("%H:%M:%S"), inline=False)
                     embedVar.add_field(name="Current time", value=datetime.now().strftime("%H:%M:%S"), inline=False)
                     embedVar.add_field(name="Result", value=result_msg, inline=False)
                     
-
-
-
                     await game.command_channel.send(embed=embedVar)
-
 
 
 # Bot commands
@@ -68,29 +61,22 @@ async def new_round(ctx, member: discord.Member, proposed_time):
 
     proposed_time_parsed = parse(proposed_time)
     proposed_time_formatted = proposed_time_parsed.strftime("%H:%M:%S")
-
-
     round_start_time = datetime.now().strftime("%H:%M:%S")
-
     
     if ctx.guild.id in bot.games:
-        await ctx.send("Sorry! A round is already in progress")
+        await ctx.send(embed=discord.Embed(Title="Error", description="Sorry! A round is already in progress", color=0xff0000))
         return
     else: 
         bot.games[ctx.guild.id] = Game(member, datetime.now(), proposed_time_parsed, ctx.channel)
 
-    
-    
-    embedVar = discord.Embed(title="New Round", description=f"Member {member.name} is being timed!", color=0x00ff00)
-    embedVar.add_field(name="Promised time", value=proposed_time_formatted, inline=False)
-    embedVar.add_field(name="Current time", value=round_start_time, inline=False)
+        embedVar = discord.Embed(title="New Round", description=f"Member {member.name} is being timed!", color=0x0000ff)
+        embedVar.add_field(name="Promised time", value=proposed_time_formatted, inline=False)
+        embedVar.add_field(name="Current time", value=round_start_time, inline=False)
+
+        await ctx.send(embed=embedVar)
 
 
-
-    await ctx.send(embed=embedVar)
-
-
-# TODO: restore as a check memebers command 
+# TODO: restore as a check members command or remove
     # vc = discord.utils.get(ctx.guild.voice_channels, name='General')
     # members = '\n - '.join([member.name for member in vc.members])
     # await ctx.send(f"Users in voice chat: \n {members}")

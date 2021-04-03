7b04916d3d14c63347429034349b42d962daff2e
Ben Dowling <ben.dowling@icloud.com>
2021-04-03 01:48:49 +0100

    Polish Round.end_round method and add dostring

--- bot/game.py
+++ bot/game.py
@@ -27,7 +27,14 @@ class Bet:
         Stake Bet: {self.stake}
         """
 
-#change to name; #pull in money
+    def set_winnings(self, winnings):
+        """Set the winnings earned from this particular bet.
+
+        Args:
+            winnings (float): The amount of Jambux won from this bet.
+        """
+        self.winnings = winnings
+
 class Player:
     def __init__(self, player_id, name, money = 100):
         self.player_id = player_id
@@ -77,8 +84,51 @@ class Round:
         Arrival Time: {"Not arrived" if self.arrival_time.isnull() else self.arrival_time}
         """
 
+    def end_round(self):
+        """End the round, setting the arrival time as the current time, getting each bet's
+        winnings, updating the player and bet objects accordingly, creating a new trial
+        balance object, and then pushing data to database.
+
+        Returns:
+            None: Chris, to decide if something should be returned.
+        """
+        self.arrival_time = np.datetime64(datetime.now())
+
+        bet_data = np.array([
+            (self.bets[i].stake, self.bets[i].time_bet)
+            for i in range(len(self.bets))
+        ])
+
+        # Keeps order of bets so can map seamlessly.
+        players = [self.bets[i].player for i in range(len(self.bets))]
+
+        stakes = player_data[:,0]
+        times_bet = player_data[:,1]
+
+        distances = calc_distances(times_bet, self.arrival_time)
+        winnings = calc_winnings(distances, stakes)
+
+        for i in range(len(self.winnings)):
+            # Add new money to player's account.
+            players[i].money += winnings[i]
+
+            # Add the winnings to each bet object.
+            self.bets[i].set_winnings(winnings[i])
+
+        # Create TB from updated player data.
+        trial_balances = [
+            Trial_Balance(player, player.money, self.arrival_time) 
+            for player in players
+        ]
+
+        # PUSH TO DB HERE. Handler?
+
+        # Return something? For Chris to decide...
+        return None
+
     def add_bet(self, player, time_bet, stake):
-        """Add a bet object to the round's list of bets. Also call the players add_bet method to add it to their list of bets.
+        """Add a bet object to the round's list of bets. 
+        Also call the players add_bet method to add it to their list of bets.
 
         Args:
             player (Player): The player object to which the bet correponds.
@@ -92,7 +142,7 @@ class Round:
         """List the bets of the round.
         
         Returns:
-            str. A string containing each bet's description.
+            str: A string containing each bet's description.
         """
         bet_descriptions = [self.bets[i].__str__() for i in range(len(bets))]
         return "\n".join(bet_descriptions)
@@ -105,38 +155,10 @@ class Round:
         """
         player_descriptions = [bets[i].player.__str__() for i in range(len(bets))]
         return "\n".join(players)
-
-    def end_round(self):
-        """TODO
-        """
-        self.arrival_time = np.datetime64(datetime.now())
-
-        player_data = np.array([
-            (self.players[i].stake, self.players[i].money, self.players[i].time_bet)
-            for i in range(len(self.players))
-        ])
-
-        stakes = player_data[:,0]
-        old_money = player_data[:,1]
-        times_bet = player_data[:,2]
-
-        distances = calc_distances(times_bet, self.arrival_time)
-        winnings = calc_winnings(distances, stakes)
-        new_money = old_money + winnings
-
-        players = [] # Pull players from FaunaDB based on the player ID.
-        # Keep order of bets so can map seamlessly.
-
-        for i in range(len(self.players)):
-            players[i].money = new_money[i]
-            # Instead post update to database?
-
-        # Return something? For Chris todo...
-        return None
  
     def calc_sandwichness(self):
-        """Get difference between when member says they will arrive vs when they actually do in seconds.
-        Not used in calculations, but probably helpful function for Chris.
+        """Get difference between when member says they will arrive vs when they actually do 
+        in seconds. Not used in calculations, but probably helpful function for Chris.
 
         Returns:
             ndarray[float]: Distance member's arrival time is away from proposed time, in minutes.            
@@ -145,7 +167,8 @@ class Round:
         return (self.proposed_time - self.arrival_time).astype(int) / _micro_second_factor
 
     def calc_distances(self, times_bet, comparative_time):
-        """Calculate the distances (in minutes) each time bet is from the comparative time (usually the arrival time).
+        """Calculate the distances (in minutes) each time bet is from the comparative time 
+        (usually the arrival time).
 
         Args:
             times_bet (ndarray[datetime64]): Times bet by each player.
@@ -161,7 +184,8 @@ class Round:
         """The score function used for calculating each bets/player's score for that round. 
 
         Args:
-            distances (ndarray[timedelta64]): The distances the times bet are away from the arrival time.
+            distances (ndarray[timedelta64]): The distances the times bet are away from 
+                the arrival time.
         
         Returns:
             ndarray[float]: The unnormalized scores for each player.
@@ -176,10 +200,12 @@ class Round:
             else np.ones(len(distances))
 
     def calc_scores(self, distances):
-        """Use the score function to calculate the scores from the distances. Subtracts score of mean to normalize.
+        """Use the score function to calculate the scores from the distances. 
+        Subtracts score of mean to normalize.
 
         Args:
-            distances (ndarray[timedelta64]): The distances the times bet are away from the arrival time.
+            distances (ndarray[timedelta64]): The distances the times bet are away from 
+                the arrival time.
 
         Returns:
             ndarray[float]: The final scores for each player.
@@ -190,7 +216,8 @@ class Round:
         """Calculate total winnings from the distances and the stakes.
 
         Args:
-            distances (ndarray[timedelta64]): The distances the times bet are away from the arrival time.
+            distances (ndarray[timedelta64]): The distances the times bet are away from 
+                the arrival time.
             stakes (ndarray[timedelta64]): The stakes (amount of money) bet.
 
         Returns:
@@ -202,6 +229,4 @@ class Round:
         return winnings
 
 if __name__ == "__main__":
-    rnd = Round("bd3dowling", datetime.now(), datetime.now(), 123)
-
-    print(rnd.__dict__)
+    return None
